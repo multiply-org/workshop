463711e462fed46cd89dc0208a89604c0ee1f9e9
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2018-08-22 18:45:55 +0200

    adapted script

--- tartu-20180828/notebooks/tartu_script_infer.py
+++ tartu-20180828/notebooks/tartu_script_infer.py
@@ -8,47 +8,47 @@ BARRAX_ROI = "POLYGON((-2.20397502663252 39.09868106889479,-1.9142106223355313 3
              "-2.20397502663252 39.09868106889479))"
 start_time = '2017-01-01'
 end_time = '2017-01-20'
-s2_data_infos = data_access_component.query(BARRAX_ROI, start_time, end_time, 'AWS_S2_L1C')
+s2_data_infos = data_access_component.query(BARRAX_ROI, start_time, end_time, 'AWS_S2_L2')
 print(s2_data_infos)
 s2_urls = data_access_component.get_data_urls_from_data_set_meta_infos(s2_data_infos)
 print(s2_urls)
-emu_urls = data_access_component.get_data_urls(BARRAX_ROI, start_time, end_time, 'ISO_MSI_A_EMU,ISO_MSI_B_EMU,WV_EMU')
-print(emu_urls)
-wv_emu_url = data_access_component.get_data_urls(BARRAX_ROI, start_time, end_time, 'WV_EMU')[0]
-print(wv_emu_url)
-aster_dem_url = data_access_component.get_data_urls(BARRAX_ROI, start_time, end_time, 'AsterDEM')[0]
-print(aster_dem_url)
-cams_urls = data_access_component.get_data_urls(BARRAX_ROI, '2017-1-16', '2017-1-16', 'CAMS')
-cams_urls.extend(data_access_component.get_data_urls(BARRAX_ROI, '2017-1-19', '2017-1-19', 'CAMS'))
-print(cams_urls)
-modis_start_time = '2017-01-13'
-modis_end_time = '2017-01-22'
-modis_urls = data_access_component.get_data_urls(BARRAX_ROI, modis_start_time, modis_end_time, 'MCD43A1.006')
-print(modis_urls)
+# emu_urls = data_access_component.get_data_urls(BARRAX_ROI, start_time, end_time, 'ISO_MSI_A_EMU,ISO_MSI_B_EMU,WV_EMU')
+# print(emu_urls)
+# wv_emu_url = data_access_component.get_data_urls(BARRAX_ROI, start_time, end_time, 'WV_EMU')[0]
+# print(wv_emu_url)
+# aster_dem_url = data_access_component.get_data_urls(BARRAX_ROI, start_time, end_time, 'AsterDEM')[0]
+# print(aster_dem_url)
+# cams_urls = data_access_component.get_data_urls(BARRAX_ROI, '2017-1-16', '2017-1-16', 'CAMS')
+# cams_urls.extend(data_access_component.get_data_urls(BARRAX_ROI, '2017-1-19', '2017-1-19', 'CAMS'))
+# print(cams_urls)
+# modis_start_time = '2017-01-13'
+# modis_end_time = '2017-01-22'
+# modis_urls = data_access_component.get_data_urls(BARRAX_ROI, modis_start_time, modis_end_time, 'MCD43A1.006')
+# print(modis_urls)
 import os
 working_dir = '/home/user/m1/'
 def create_dir(dir):
     if not os.path.exists(dir):
         os.makedirs(dir)
 create_dir(working_dir)
-s2_l1c_dir = '{}/s2_l1c'.format(working_dir)
-create_dir(s2_l1c_dir)
-emus_dir = '{}/emus'.format(working_dir)
-create_dir(emus_dir)
-cams_dir = '{}/cams'.format(working_dir)
-create_dir(cams_dir)
-modis_dir = '{}/modis'.format(working_dir)
-create_dir(modis_dir)
 s2_l2_dir = '{}/s2_l2'.format(working_dir)
-create_dir(s2_l2_dir)
+# create_dir(s2_l1c_dir)
+# emus_dir = '{}/emus'.format(working_dir)
+# create_dir(emus_dir)
+# cams_dir = '{}/cams'.format(working_dir)
+# create_dir(cams_dir)
+# modis_dir = '{}/modis'.format(working_dir)
+# create_dir(modis_dir)
+# s2_l2_dir = '{}/s2_l2'.format(working_dir)
+# create_dir(s2_l2_dir)
 from multiply_orchestration import create_sym_links
-create_sym_links(s2_urls, s2_l1c_dir)
-create_sym_links(emu_urls, emus_dir)
-create_sym_links(cams_urls, cams_dir)
-create_sym_links(modis_urls, modis_dir)
+create_sym_links(s2_urls, s2_l2_dir)
+# create_sym_links(emu_urls, emus_dir)
+# create_sym_links(cams_urls, cams_dir)
+# create_sym_links(modis_urls, modis_dir)
 
-import glob
-s2_dirs = glob.glob(s2_l1c_dir + "/*/*/*/*/*/*/*")
+# import glob
+# s2_dirs = glob.glob(s2_l1c_dir + "/*/*/*/*/*/*/*")
 
 # processor_dir = '/software/atmospheric_correction-0.8/multiply_atmospheric_corection'
 # os.system("sudo ln -s "+processor_dir+"/data ./data")
@@ -72,7 +72,7 @@ variables = 'n, cab, car, cbrown, cw, cm, lai, ala, bsoil, psoil'
 #     PE = PriorEngine(config=configuration_file, datestr=time.strftime('%Y-%m-%d'), variables=variables)
 #     priors = PE.get_priors()
 #     time = time + datetime.timedelta(days=1)
-# priors_dir = '{}/priors'.format(working_dir)
+priors_dir = '{}/priors'.format(working_dir)
 # create_dir(priors_dir)
 # os.system("mv *.vrt " +priors_dir+"/")
 
@@ -102,7 +102,7 @@ inference_engine_call = ( "python "+inference_engine_dir+"/inference_engine.py"
           + " -em " + s2_emulators_dir
           + " -p " + variables
           + ((" -ps " + previous_state) if previous_state != 'none' else "")
-          + " -pd " + priors_date_dir
+          + " -pd " + priors_dir
           + " -d " + s2_l2_dir
           + " -sm " + state_mask
           + " -o " + biophys_dir
