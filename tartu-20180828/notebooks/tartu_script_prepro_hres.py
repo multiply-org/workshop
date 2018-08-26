538c8ec299297bd8f5172af57c9f512f3f61db44
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2018-08-26 22:01:13 +0200

    script change

--- tartu-20180828/notebooks/tartu_script_prepro_hres.py
+++ tartu-20180828/notebooks/tartu_script_prepro_hres.py
@@ -5,8 +5,8 @@ data_access_component.get_provided_data_types()
 BARRAX_ROI = "POLYGON((-2.20397502663252 39.09868106889479,-1.9142106223355313 39.09868106889479," \
              "-1.9142106223355313 38.94504502508093,-2.20397502663252 38.94504502508093," \
              "-2.20397502663252 39.09868106889479))"
-start_time = '2017-01-10'
-end_time = '2017-01-20'
+start_time = '2017-06-05'
+end_time = '2017-06-10'
 s2_data_infos = data_access_component.query(BARRAX_ROI, start_time, end_time, 'AWS_S2_L1C')
 print(s2_data_infos)
 s2_urls = data_access_component.get_data_urls_from_data_set_meta_infos(s2_data_infos)
@@ -17,11 +17,11 @@ wv_emu_url = data_access_component.get_data_urls(BARRAX_ROI, start_time, end_tim
 print(wv_emu_url)
 aster_dem_url = data_access_component.get_data_urls(BARRAX_ROI, start_time, end_time, 'AsterDEM')[0]
 print(aster_dem_url)
-cams_urls = data_access_component.get_data_urls(BARRAX_ROI, '2017-01-16', '2017-01-16', 'CAMS')
-cams_urls.extend(data_access_component.get_data_urls(BARRAX_ROI, '2017-01-19', '2017-01-19', 'CAMS'))
+cams_urls = data_access_component.get_data_urls(BARRAX_ROI, '2017-06-05', '2017-06-05', 'CAMS')
+cams_urls.extend(data_access_component.get_data_urls(BARRAX_ROI, '2017-06-08', '2017-06-08', 'CAMS'))
 print(cams_urls)
-modis_start_time = '2017-01-13'
-modis_end_time = '2017-01-22'
+modis_start_time = '2017-06-02'
+modis_end_time = '2017-06-11'
 modis_urls = data_access_component.get_data_urls(BARRAX_ROI, modis_start_time, modis_end_time, 'MCD43A1.006')
 print(modis_urls)
 import os
@@ -49,19 +49,19 @@ create_sym_links(modis_urls, modis_dir)
 import glob
 s2_dirs = glob.glob(s2_l1c_dir + "/*/*/*/*/*/*/*")
 
-s2_dir = s2_dirs[1]
-input_parts = s2_dir.split('/')
-s2_l2_product_name = 'S2-{}{}{}'.format(input_parts[-4], input_parts[-3], input_parts[-2])
+for s2_dir in s2_dirs:
+    input_parts = s2_dir.split('/')
+    s2_l2_product_name = 'S2-{}{}{}'.format(input_parts[-4], input_parts[-3], input_parts[-2])
 
-processor_dir = '/software/atmospheric_correction-0.8/multiply_atmospheric_corection'
-os.system("sudo ln -s "+processor_dir+"/data ./data")
-command = "PYTHONPATH=$PYTHONPATH:" + processor_dir + "/util python " + processor_dir + "/Sentinel2_AtmoCor.py -f " + s2_dir + "/ -m " + modis_dir + " -e " + emus_dir + " -c " + cams_dir + " -w " + wv_emu_url + " -d " + aster_dem_url
-os.system(command)
-s2_l2_product_dir = s2_l2_dir + "/" + s2_l2_product_name
-create_dir(s2_l2_product_dir)
-os.system('sudo cp -b ' + s2_dir + '/metadata.xml ' + s2_l2_product_dir)
-os.system("rm $(find "+s2_dir+" -type l)")
-os.system('mv ' + s2_dir + '/* ' + s2_l2_product_dir)
+    processor_dir = '/software/atmospheric_correction-0.8/multiply_atmospheric_corection'
+    os.system("sudo ln -s "+processor_dir+"/data ./data")
+    command = "PYTHONPATH=$PYTHONPATH:" + processor_dir + "/util python " + processor_dir + "/Sentinel2_AtmoCor.py -f " + s2_dir + "/ -m " + modis_dir + " -e " + emus_dir + " -c " + cams_dir + " -w " + wv_emu_url + " -d " + aster_dem_url
+    os.system(command)
+    s2_l2_product_dir = s2_l2_dir + "/" + s2_l2_product_name
+    create_dir(s2_l2_product_dir)
+    os.system('sudo cp -b ' + s2_dir + '/metadata.xml ' + s2_l2_product_dir)
+    os.system("rm $(find "+s2_dir+" -type l)")
+    os.system('mv ' + s2_dir + '/* ' + s2_l2_product_dir)
 # data_access_component.put(s2_l2_product_dir)
 # maybe later for cleanup
 # os.system("rm -rf "+s2_dir)
